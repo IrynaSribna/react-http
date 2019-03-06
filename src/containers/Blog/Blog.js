8d3d5d6c7322ba159822c512501a79b23f70fbd7
iryna.sribna <iryna.sribna@awin.com>
2019-03-06 12:20:38 +0100

    Make Posts clickable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts') //get returns Promise
@@ -24,9 +25,18 @@ class Blog extends Component {
             //because javaScipt will not wait till this call gets finished
         });
     }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />;
+            return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author} 
+                clicked={() => this.postSelectedHandler(post.id)}/>;
         })
         return (
             <div>
@@ -34,7 +44,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
