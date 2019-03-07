d2e62bb0ca77ddef78f41e3bdb46b53d8f13c832
iryna.sribna <iryna.sribna@awin.com>
2019-03-07 12:17:07 +0100

    Add interseptors to execute code globally

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+
+//this is for requests coming from application
+axios.interceptors.request.use(requestSettings => {
+    console.log(requestSettings);
+
+    // Edit request settings can be changed and then returned
+    return requestSettings; //we blocking request if we do not do return
+}, error => {
+    console.log(error); //we can for instanse log it and forward futher
+    return Promise.reject(error); //we need this to be able to hadle in the app within catch
+});
+
+//this is for responses coming from server
+axios.interceptors.response.use(responseSettings => {
+    console.log(responseSettings);
+
+    // Edit response settings can be changed and then returned
+    return responseSettings; //we blocking request if we do not do return
+}, error => {
+    console.log(error); //we can for instanse log it and forward futher
+    return Promise.reject(error); //we need this to be able to hadle in the app within catch
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
