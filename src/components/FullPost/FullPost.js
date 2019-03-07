f9a550c252f20461b9c5bc94d63857c803a25d77
iryna.sribna <iryna.sribna@awin.com>
2019-03-07 08:21:27 +0100

    Fetch data on update in selected posts

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null
+    }
+
+    componentDidUpdate () {
+        if (this.props.id) {
+            //the if below is needed so that componentDidUpdate is not loaded
+            // in the infinite loop of request but only if we select a new post (this can be seen in the Network tab)
+            if (!this.state.loadedPost 
+                || (this.state.loadedPost 
+                    && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        this.setState({loadedPost: response.data})
+                    });
+            }    
+        }
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if (this.props.id) {
+            post = <p style={{textAlign: 'center'}}> Loading...</p>;
+        }
+        if(this.state.loadedPost) {  //we can already obtain the id but the date are still not loaded 
+            //as the loading happens async  
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
