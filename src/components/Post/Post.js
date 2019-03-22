528f075132a18b14dbfdf26a7da953cc31955e23
iryna.sribna <iryna.sribna@awin.com>
2019-03-22 11:10:35 +0100

    Add withRoute component to get Route props from Route containing components

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './Post.css';
 
 const post = (props) => (
+    
     <article className="Post" onClick={props.clicked}>
         <h1>{props.title}</h1>
         <div className="Info">
@@ -11,4 +13,4 @@ const post = (props) => (
     </article>
 );
 
-export default post;
+export default withRouter(post);
