dc179c7f4ed6b35d23a04c8e2a2d75a8d6a92e79
iryna.sribna <iryna.sribna@awin.com>
2019-03-28 13:33:56 +0100

    Dynamic nested routes: fix load of the full post

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,14 +8,23 @@ class FullPost extends Component {
         loadedPost: null
     }
 
+    componentWillUpdate() {
+        console.log(this.props)
+        this.loadData();
+    }
+
     componentDidMount () {
         console.log(this.props)
+        this.loadData();
+    }
+
+    loadData() {
         if (this.props.match.params.postId) {
             //the if below is needed so that componentDidUpdate is not loaded
             // in the infinite loop of request but only if we select a new post (this can be seen in the Network tab)
             if (!this.state.loadedPost 
                 || (this.state.loadedPost 
-                    && this.state.loadedPost.id !== this.props.id)) {
+                    && this.state.loadedPost.id != this.props.match.params.postId)) { //!== changed to != to compare values as numbers
                 axios.get('/posts/' + this.props.match.params.postId)
                     .then(response => {
                         this.setState({loadedPost: response.data})
@@ -33,7 +42,7 @@ class FullPost extends Component {
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if (this.props.id) {
+        if (this.props.match.params.postId) {
             post = <p style={{textAlign: 'center'}}> Loading...</p>;
         }
         if(this.state.loadedPost) {  //we can already obtain the id but the date are still not loaded 
