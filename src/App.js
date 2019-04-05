b920e0c8d4dbd91b66dbca1e49a3f09219d37651
iryna.sribna <iryna.sribna@awin.com>
2019-04-05 08:18:00 +0200

    Laziness/Suspense component. From React 16.6

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      //React.Fragment was intoduced to not use Aux
       <BrowserRouter>
         <div className="App">
-          <Blog />
-        </div>
-      </BrowserRouter>
+            <Blog />
+          </div>
+        </BrowserRouter>
     );
   }
 }
@@ -18,3 +19,12 @@ class App extends Component {
 export default App;
 
 //the following packages were installer: npm --save react-routing react-routing-dom
+
+
+//Laziness start from React 16.6
+//after imports but before class declaration
+// const Posts = React.lazy(()=> import('./Posts'))
+//inside the class
+// <Route path="/posts" render={() => (<Suspense fallback={<div>Loading...</div>})}> <Posts/> </Suspense>/>
+//laziness and suspense can be applied not only to Routes but to other components too, for example to buttons
+
